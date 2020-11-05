3468759a682f09beeff3187b7000034e516f7491
Augusto Schnorr <alcsaw@hotmail.com>
2020-11-05 13:30:14 -0300

    Adds appointments routes

--- nível02/node-ts/src/routes/index.ts
+++ nível02/node-ts/src/routes/index.ts
@@ -1,20 +1,8 @@
 import { Router } from 'express';
+import appointmenstRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ status: 'OK' });
-});
-
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
+routes.use('/appointments', appointmenstRouter);
 
 export default routes;
